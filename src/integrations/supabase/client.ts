93bcc7a3bf2104ead0adf09f87687b818712e902
Naveen Raj <naveenraj@Naveens-MacBook-Air.local>
2026-05-05 22:20:47 +0530

    udate0.4

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://rtlbqcostdpkaisnkwjm.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0bGJxY29zdGRwa2Fpc25rd2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzOTM4NjQsImV4cCI6MjA3OTk2OTg2NH0._2pjkDazKjoPArrKtCkSYlV5Mv8dQ17H1cwiPgfyaHs";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
